173da5dc43be392406e64249293b6ed90cd23010
Thibault CHANUT <t.chanut@game-sup.fr>
2026-02-06 12:26:34 +0100

    procedural "working" + change in teleport

--- Assets/Scripts/S_Teleporter.cs
+++ Assets/Scripts/S_Teleporter.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Net;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,60 +8,22 @@ public class Teleporter : MonoBehaviour
 {
 
     public GameObject tpLocation;
+    public S_TeleporterHandler _teleporterHandler;
 
     [SerializeField] private List<GameObject> _enemiesInZone;
-    [SerializeField][Tooltip("Only put the 'P_TpPoint' that you do not want to use in this list")] private List<GameObject> teleporterToNotUse;
     
     private GameObject _player;
     private S_CharacterCollisionHandler _collisionHandler;
-    private List<GameObject> _teleporters;
 
     private void Start()
     {
-        _teleporters = new List<GameObject>();
         if (tpLocation == null)
         {
-            SearchTpPoints();
+            Debug.LogError("Teleporter: tpLocation is null");
         }
     }
 
-    private void SearchTpPoints()
-    {
-        List<GameObject> gameObjectsToRemove = new List<GameObject>();
-        float minDistanceTpDistance = 0.0f;
-        GameObject minDistanceTp = null;
-        _teleporters.AddRange(GameObject.FindGameObjectsWithTag("TpPoint"));
-        foreach (GameObject teleporterRef in _teleporters)
-        {
-            foreach (GameObject teleporterUnused in teleporterToNotUse)
-            {
-                if (teleporterUnused == teleporterRef)
-                {
-                    gameObjectsToRemove.Add(teleporterRef);
-                }
-            }
-        }
-        foreach (GameObject teleporterToremove in gameObjectsToRemove)
-        {
-            _teleporters.Remove(teleporterToremove);
-        }
-        foreach (GameObject teleporterLasting in _teleporters)
-        {
-            if (minDistanceTpDistance == 0f)
-            {
-                minDistanceTpDistance = Vector3.Distance(transform.position, teleporterLasting.transform.position);
-                minDistanceTp = teleporterLasting;
-                continue;
-            }
-            if (minDistanceTpDistance > Vector3.Distance(transform.position, teleporterLasting.transform.position))
-            {
-                minDistanceTpDistance = Vector3.Distance(transform.position, teleporterLasting.transform.position);
-                minDistanceTp = teleporterLasting;
-            }
-            tpLocation = minDistanceTp;
-        }
-    }
-    
+
     private void OnTriggerEnter(Collider collision)
     {
         if (_player != null && collision.gameObject.GameObject() == _player)
