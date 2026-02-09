c3cdb69bfe1859434d9fad68e630489a54729abb
Thibault CHANUT <t.chanut@game-sup.fr>
2026-02-09 16:29:05 +0100

    push enemy & base combat idea + package for VFX

--- Assets/Scripts/S_Rogue_Combat.cs
+++ Assets/Scripts/S_Rogue_Combat.cs
@@ -1,15 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class S_Rogue_Combat : MonoBehaviour
 {
     private S_HP_Component _characterHealthRef;
     private S_HP_Component _targetHealthRef;
+    private S_Rogue_Inputs _inputsManager;
+    [SerializeField] List<Collider> _attackHitBoxes;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        _characterHealthRef = this.GetComponent<S_HP_Component>();
-        
+        _characterHealthRef = GetComponent<S_HP_Component>();
+        _inputsManager = GetComponent<S_Rogue_Inputs>();
     }
 
     // Update is called once per frame
