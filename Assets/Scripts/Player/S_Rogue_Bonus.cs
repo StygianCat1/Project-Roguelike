87a60001bf3131b347e00756645bcf709a357472
Thibault CHANUT <t.chanut@game-sup.fr>
2026-02-11 20:57:52 +0100

    base for save between game

--- Assets/Scripts/Player/S_Rogue_Bonus.cs
+++ Assets/Scripts/Player/S_Rogue_Bonus.cs
@@ -5,24 +5,42 @@ public class S_Rogue_Bonus : MonoBehaviour
     [Range(0, 3)] public int perversionUpgradeLevel;
     [Range(0, 3)] public int luckyGamblerUpgradeLevel;
     [Range(0, 3)] public int avidityUpgradeLevel;
-    [Range(0, 3)] public int pangryKaoriUpgradeLevel;
-    private int baseKaoriCooldown;
+    [Range(0, 3)] public int angryKaoriUpgradeLevel;
+    public int baseKaoriCooldown;
     
-    [Range(0, 3)] public int LuckyShotCooldown;
+    [Range(0, 3)] public int luckyShotCooldown;
     public int luckyshotRate = 0;
-    
-    
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        perversionUpgradeLevel = S_GameManager.perversionUpgradeLevelSave;
+        luckyGamblerUpgradeLevel =S_GameManager.luckyGamblerUpgradeLevelSave;
+        avidityUpgradeLevel = S_GameManager.avidityUpgradeLevelSave;
+        angryKaoriUpgradeLevel = S_GameManager.angryKaoriUpgradeLevelSave;
+        luckyShotCooldown = S_GameManager.luckyShotCooldownSave;
+        
+        //VerifyAllBonuses();
+    }
+    
+    public void VerifyAllBonuses()
+    {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PerversionLevel()
     {
+        
+    }
+    
+    public void SaveAllBonuses()
+    {
+        S_GameManager.perversionUpgradeLevelSave = perversionUpgradeLevel;
+        S_GameManager.luckyGamblerUpgradeLevelSave = luckyGamblerUpgradeLevel;
+        S_GameManager.avidityUpgradeLevelSave = avidityUpgradeLevel;
+        S_GameManager.angryKaoriUpgradeLevelSave = angryKaoriUpgradeLevel;
+        S_GameManager.luckyShotCooldownSave = luckyShotCooldown;
         
     }
 }
---
+++ Assets/Scripts/Save/S_GameManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class S_GameManager : MonoBehaviour
+{
+    public static int outGameMoneySave;
+    
+    public static int perversionUpgradeLevelSave;
+    public static int luckyGamblerUpgradeLevelSave;
+    public static int avidityUpgradeLevelSave;
+    public static int angryKaoriUpgradeLevelSave;
+    public static int luckyShotCooldownSave;
+}
