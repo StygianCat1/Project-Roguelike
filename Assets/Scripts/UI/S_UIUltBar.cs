24b7e4033c5900f64e7d315e9c1c08b7765a0a56
NolanP <n.pottier@game-sup.fr>
2026-02-13 16:33:26 +0100

    UptadeUICode

---
+++ Assets/Scripts/UI/S_UIUltBar.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_UIUltBar : MonoBehaviour
+
+
+{
+
+    [SerializeField] public float ultBar = 0f;
+    [SerializeField] public float maxUltBar = 100f;
+    public Image ultBarImage;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            ultBar = Mathf.Clamp(ultBar, 0f, maxUltBar);
+            ultBarImage.fillAmount = ultBar / maxUltBar;
+        }
+    }
+    
+    public void DownUltButton(int damageAmount)
+    {
+        ultBar -= damageAmount;
+    }
+    
+    public void UpUltButton(int healAmount)
+    {
+        ultBar += healAmount;
+    }
+}
